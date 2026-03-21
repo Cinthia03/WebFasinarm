e9808690f10c081f3a5d2d9300eb58b4f2d2469d
Cinthia <cinthia2003maria@gmail.com>
2026-03-21 16:46:55 -0500

    Cambios conf supabase

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,7 @@
 export const environment = {
   production: false,
-  supabaseUrl: 'https://mzvufwmocykzsxfsrspx.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16dnVmd21vY3lrenN4ZnNyc3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NzQ5NDksImV4cCI6MjA4NzQ1MDk0OX0.BP5u0vxNalockMbYx1Ftt0-MKQjQpTYYsm9gby2Lt1s'
+  //supabaseUrl: 'https://mzvufwmocykzsxfsrspx.supabase.co',
+  //supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im16dnVmd21vY3lrenN4ZnNyc3B4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4NzQ5NDksImV4cCI6MjA4NzQ1MDk0OX0.BP5u0vxNalockMbYx1Ftt0-MKQjQpTYYsm9gby2Lt1s'
+  supabaseUrl: 'https://iyvoaitqrksvliieqmjj.supabase.co',
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml5dm9haXRxcmtzdmxpaWVxbWpqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzQxMjI2ODQsImV4cCI6MjA4OTY5ODY4NH0.maQlFJuS7A9i3wDHm1VkS9CurU8GVIjd8vUN-K1wgQ8'
 };
